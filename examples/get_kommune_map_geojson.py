f85db2fae65bb74d2128ca15949374cb8e0b9277
Troels Schwarz-Linnet <tlinnet@gmail.com>
2017-12-04 14:58:00 +0100

    Test JSON scripts

---
+++ examples/get_kommune_map_geojson.py
@@ -0,0 +1,30 @@
+"""# Get import"""
+try:
+    from kmdvalg import kommune
+except ImportError:
+    import sys, os
+    sys.path.append( os.getcwd()+os.sep+"..")
+    print(sys.path)
+
+"""# Get import"""
+from kmdvalg import kommune
+import bokeh.plotting as bplt
+
+
+"""# Get kommune map"""
+kmap = kommune.kmap(make=True)
+
+"""# Get plot import"""
+from bqplot import pyplot as plt
+from bqplot import *
+import os
+
+"""# Get plot"""
+map_fig = plt.figure(title="Test")
+map_res = plt.geo(map_data=topo_load(os.getcwd()+os.sep+"Kommune.json"), stroke_color='black')
+
+"""# Show output"""
+# Get output. Either to Jupyter notebook or html file 
+if True:
+    if kommune.check_isnotebook():
+        display(map_fig)
